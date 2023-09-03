a438d5a2c89b56c2aff8e9f8e026869b864b6ed4
monicakrystal <131429507+monicakrystal@users.noreply.github.com>
2023-09-02 17:01:30 -0700

    main.js

---
+++ ISBN search/js/main.js
@@ -0,0 +1,27 @@
+document.querySelector('button').addEventListener('click', getFetch)
+
+document.querySelector('h2').innerText = localStorage.getItem('books')
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  console.log(choice)
+  const url = `https://openlibrary.org/isbn/${choice}.json`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data.title)
+        if (!localStorage.getItem('books')){
+            localStorage.setItem('books', data.title)
+        }else{
+        let allBooks = localStorage.getItem('books') + " ; " + data.title
+        localStorage.setItem('books', allBooks)
+        }
+
+        document.querySelector('h2').innerText = localStorage.getItem('books')
+              
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
